9f7d4dba49150d29a8f5639734b978adda6c14ff
TikhomirovEgor <e.tihomirov3@stud.uniyar.ac.ru>
2024-06-05 00:52:45 +0300

    crossword3
    step 3

--- Crossword/Crossword.cpp
+++ Crossword/Crossword.cpp
@@ -13,7 +13,102 @@ struct Point {
         y = b;
     }
 };
+class Button {
+public:
+    float x;
+    float y;
+    float width;
+    float height;
+    const char* text;
+    int sz = 0;
+    Button() = default;
+    Button(float x0, float y0, float width0, float height0, const char* text0) {
+        x = x0;
+        y = y0;
+        width = width0;
+        height = height0;
+        text = text0;
+        sz = strlen(text0);
+    }
+    Button(float x0, float y0, float width0, float height0, int int0) {
+        x = x0;
+        y = y0;
+        width = width0;
+        height = height0;
+        int s = 1;
+        while (int0 / (10 * s) > 0) {
+            ++s;
+        }
+        sz = s;
+        char* char0 = new char[s];
+        for (int i = 0; i < s; ++i) {
+            if (int0 > 9) {
+                char0[i] = char(int0 / (pow(10, (s - i - 1))) + 48);
+                int g = (pow(10, (s - i - 1)));
+                int0 = int0 % g;
 
+            }
+            else {
+                char0[i] = char(int0 % 10 + 48);
+
+            }
+        }
+
+        text = char0;
+
+
+    }
+    ~Button() {
+        delete[] text;
+    }
+    void drawButton() {
+        glBegin(GL_POLYGON);
+        glColor3f(0.666, 0.5, 0.9);
+        glVertex2f(x, y);
+        glVertex2f(x + width, y);
+        glVertex2f(x + width, y + height);
+        glVertex2f(x, y + height);
+        glEnd();
+
+        glColor3f(1, 0.9, 0);
+
+        float textX = x + 0.1;
+        float textY = y + 1;
+
+        glRasterPos2f(textX, textY);
+        for (int i = 0; i < sz; ++i) {
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
+        }
+
+        glFlush();
+    }
+    void drawButton(int d) {
+        glBegin(GL_POLYGON);
+        glColor3f(0.666, 0.5, 0.9);
+        glVertex2f(x, y);
+        glVertex2f(x + width, y);
+        glVertex2f(x + width, y + height);
+        glVertex2f(x, y + height);
+        glEnd();
+
+        glColor3f(1, 0.9, 0);
+
+        float textX = x + 0.1;
+        float textY = y + 1;
+
+        glRasterPos2f(textX, textY);
+        for (int i = 0; i < sz; ++i) {
+
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
+        }
+
+        glFlush();
+    }
+    bool isButtonHovered(Point press) {
+        return press.x >= x && press.x <= x + width &&
+            press.y >= y && press.y <= y + height;
+    }
+};
 struct cell
 {
     float x;
@@ -21,7 +116,7 @@ struct cell
     float width;
     float height;
     char ans;
-    char current = '*';
+    char current = ' ';
     char num[2] = { '*','*' };
     int sz = 0;
     cell() = default;
@@ -40,27 +135,27 @@ struct cell
     }
     void drawButton() {
         glBegin(GL_POLYGON);
-        glColor3f(0.666, 0.5, 0.9);
+        glColor3f(0, 0.45, 0.9);
         glVertex2f(x, y);
         glVertex2f(x + width, y);
         glVertex2f(x + width, y + height);
         glVertex2f(x, y + height);
         glEnd();
 
-        glColor3f(1, 0.9, 0);
+        glColor3f(1, 0.6, 0);
 
-        float textX = x + 0.1;
+        float textX = x + 0.3;
         float textY = y + 0.7;
 
         glRasterPos2f(textX, textY);
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ans);
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, current);
         if (num[0] != '*') {
-            float textX = x + 0.1;
+            float textX = x + 0.3;
             float textY = y + 3;
             glRasterPos2f(textX, textY);
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, num[0]);
 
-            textX = x + 1;
+            textX = x + 1.2;
 
             glRasterPos2f(textX, textY);
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, num[1]);
@@ -96,9 +191,25 @@ struct cell
     }
 };
 
+
+enum MenuState
+{
+    MAIN,
+    PLAY,
+    WIN,
+};
+MenuState currentMenuState = MAIN;
+
+
 struct H {
     static std::vector<cell*> ce;
+    static Button cr1;
+    static Button cr2;
+    static Button cr3;
+    static Button to_manu;
+    static Button ok;
 }; std::vector<cell*> H::ce;
+Button H::cr1(20, 50, 11, 5, "crossword 1"), H::cr2(70, 50, 11, 5, "crossword 2"), H::cr3(120, 50, 11, 5, "crossword 3"), H::to_manu(130, 5, 10, 5, "to manu");
 
 std::vector<cell*> cells(const std::vector<std::vector<char>>& fld, const int& s, const int& c) {
     std::vector<cell*> cells;
@@ -124,12 +235,33 @@ std::vector<cell*> cells(const std::vector<std::vector<char>>& fld, const int& s
     return cells;
 }
 
-
+void renderBitmapString(float x, float y, void* font, std::string string) {
+    glRasterPos2f(x, y);
+    for (char c : string) {
+        glutBitmapCharacter(font, c);
+    }
+}
 
 void DrawField() {
-    for (int i = 0; i < H::ce.size(); ++i) {
-        (*H::ce[i]).drawButton();
+
+
+    switch (currentMenuState)
+    {
+    case MAIN:
+        renderBitmapString(55, 80, GLUT_BITMAP_TIMES_ROMAN_24, "Choose a crossword puzzle");
+        H::cr1.drawButton();
+        H::cr2.drawButton();
+        H::cr3.drawButton();
+        break;
+    case PLAY:
+        H::to_manu.drawButton();
+        for (int i = 0; i < H::ce.size(); ++i) {
+            (*H::ce[i]).drawButton();
+        }
+        break;
     }
+
+
 
 
 }
@@ -145,10 +277,89 @@ void display() {
 
 }
 
+void MousePressed(int button, int state, int x, int y) {
+    float x0 = x / 9.7;
+    float y0 = (1000 - y) / 10;
+    Point xy(x0, y0);
+
+
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        switch (currentMenuState) {
+        case MAIN:
+            if (H::cr1.isButtonHovered(xy)) {
+                std::ifstream fin("crossword_field1.txt");
+                std::vector<std::vector<char>> field;
+                std::string s;
+                int cl = 0;
+                int st = 0;
+                while (fin >> s) {
+                    cl = s.size();
+                    std::vector<char> str;
+                    for (int i = 0; i < s.size(); ++i) {
+                        str.push_back(s[i]);
+                    }
+                    field.push_back(str);
+                    ++st;
+                }
+                H::ce = cells(field, st, cl);
+                fin.close();
+                currentMenuState = PLAY;
+            }
+            else if (H::cr2.isButtonHovered(xy)) {
+                std::ifstream fin("crossword_field2.txt");
+                std::vector<std::vector<char>> field;
+                std::string s;
+                int cl = 0;
+                int st = 0;
+                while (fin >> s) {
+                    cl = s.size();
+                    std::vector<char> str;
+                    for (int i = 0; i < s.size(); ++i) {
+                        str.push_back(s[i]);
+                    }
+                    field.push_back(str);
+                    ++st;
+                }
+                H::ce = cells(field, st, cl);
+                fin.close();
+                currentMenuState = PLAY;
+            }
+            else if (H::cr3.isButtonHovered(xy)) {
+                std::ifstream fin("crossword_field3.txt");
+                std::vector<std::vector<char>> field;
+                std::string s;
+                int cl = 0;
+                int st = 0;
+                while (fin >> s) {
+                    cl = s.size();
+                    std::vector<char> str;
+                    for (int i = 0; i < s.size(); ++i) {
+                        str.push_back(s[i]);
+                    }
+                    field.push_back(str);
+                    ++st;
+                }
+                H::ce = cells(field, st, cl);
+                fin.close();
+                currentMenuState = PLAY;
+            }
+            display();
+            break;
+        case PLAY:
+            if (H::to_manu.isButtonHovered(xy)) {
+                currentMenuState = MAIN;
+            }
+            display();
+            break;
+        }
+
+
+    }
+}
 
 void init() {
     // Цвет фона
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClearColor(0.1, 0.2111, 0.4511, 0.8);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     // Ортогональная проекция
@@ -162,21 +373,7 @@ void init() {
 
 int main(int argc, char** argv)
 {
-    std::ifstream fin("srossword_field.txt");
-    std::vector<std::vector<char>> field;
-    std::string s;
-    int cl = 0;
-    int st = 0;
-    while (fin >> s) {
-        cl = s.size();
-        std::vector<char> str;
-        for (int i = 0; i < s.size(); ++i) {
-            str.push_back(s[i]);
-        }
-        field.push_back(str);
-        ++st;
-    }
-    H::ce = cells(field, st, cl);
+
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
@@ -185,7 +382,7 @@ int main(int argc, char** argv)
     glutCreateWindow("crossword");
     init();
     glutDisplayFunc(display);
-
+    glutMouseFunc(MousePressed);
     glutMainLoop();
 
     return 0;
